b0e359af2e0c298c755bb3a731f150c36d82fa15
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2023-11-08 21:16:49 +0700

    .

--- Lab-5/index.js
+++ Lab-5/index.js
@@ -1,5 +1,5 @@
-let width = 1300;
-let height = 1300;
+let width = 1500;
+let height = 800;
 let spacing = 100;
 
 let info = d3
@@ -21,19 +21,19 @@ let svg = d3
 d3.csv(
   "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 ).then(function (data) {
-  let date = "5/4/20"; // Modify the date
+  let date = "3/20/20";
   let dataByDate = data.map(function (d) {
     return {
       country: d["Country/Region"],
       lat: parseFloat(d["Lat"]),
-      long: parseFloat(d["Long"]),
+      long: parseFloat( d["Long"]),
       date: date,
       confirmed: parseInt(d[date]),
     };
   });
+  console.log(dataByDate);
   draw(dataByDate);
 });
-// Draw scatterplot with date
 function draw(data) {
   let minLat = d3.min(data, function (d) {
     return d.lat;
@@ -41,20 +41,18 @@ function draw(data) {
   let maxLat = d3.max(data, function (d) {
     return d.lat;
   });
-  // Modify the minLong
   let minLong = d3.min(data, function (d) {
     return d.long;
   });
-  // Modify the maxLong
   let maxLong = d3.max(data, function (d) {
     return d.long;
   });
 
   console.log(minLat, maxLat, minLong, maxLong);
 
-  minLat = Math.floor(minLat);
+  minLat = Math.ceil(minLat);
   maxLat = Math.ceil(maxLat);
-  minLong = Math.floor(minLong);
+  minLong = Math.ceil(minLong);
   maxLong = Math.ceil(maxLong);
 
   console.log(minLat, maxLat, minLong, maxLong);
@@ -71,20 +69,20 @@ function draw(data) {
   var xAxis = d3.axisBottom(xScale);
   var yAxis = d3.axisLeft(yScale);
 
-  svg
-    .append("g")
-    .attr("class", "grid")
-    .attr("transform", `translate(0, ${height - spacing / 2})`)
-    .call(
-      d3
-        .axisBottom(xScale)
-        .tickSize(-height + spacing / 2)
-        .tickFormat("")
-        .tickSizeOuter(0)
-        .tickSizeInner(-height + spacing / 2)
-        .tickPadding(10)
-        .tickFormat("")
-    );
+  // svg
+  //   .append("g")
+  //   .attr("class", "grid")
+  //   .attr("transform", `translate(0, ${height - spacing / 2})`)
+  //   .call(
+  //     d3
+  //       .axisBottom(xScale)
+  //       .tickSize(-height + spacing / 2)
+  //       .tickFormat("")
+  //       .tickSizeOuter(0)
+  //       .tickSizeInner(-height + spacing / 2)
+  //       .tickPadding(10)
+  //       .tickFormat("")
+  //   );
 
   svg
     .append("g")
@@ -101,59 +99,58 @@ function draw(data) {
         .tickFormat("")
     );
 
-  svg
-    .append("g")
-    .attr("id", "xAxis")
-    .attr("transform", `translate(0, ${(height - spacing) / 2})`)
-    .call(xAxis)
-    .selectAll("text")
-    .style("font-size", "14px")
-    .style("font-weight", "bold")
-    .filter(function (d) {
-      return d === 0;
-    })
-    .attr("transform", "translate(-10,0)")
-    .style("text-anchor", "end");
+  // svg
+  //   .append("g")
+  //   .attr("id", "xAxis")
+  //   .attr("transform", `translate(0, ${(height - spacing) / 2})`)
+  //   .call(xAxis)
+  //   .selectAll("text")
+  //   .style("font-size", "14px")
+  //   .style("font-weight", "bold")
+  //   .filter(function (d) {
+  //     return d === 0;
+  //   })
+  //   .attr("transform", "translate(-10,0)")
+  //   .style("text-anchor", "end");
 
-  svg
-    .append("g")
-    .attr("id", "yAxis")
-    .attr("transform", `translate(${(width - spacing) / 2}, 0)`)
-    .call(yAxis)
-    .selectAll("text")
-    .style("font-size", "14px")
-    .style("font-weight", "bold")
-    .attr("transform", "translate(0,-3)")
-    .filter(function (d) {
-      return d === 0;
-    })
-    .remove();
+  // svg
+  //   .append("g")
+  //   .attr("id", "yAxis")
+  //   .attr("transform", `translate(${(width - spacing) / 2}, 0)`)
+  //   .call(yAxis)
+  //   .selectAll("text")
+  //   .style("font-size", "14px")
+  //   .style("font-weight", "bold")
+  //   .attr("transform", "translate(0,-3)")
+  //   .filter(function (d) {
+  //     return d === 0;
+  //   })
+  //   .remove();
 
-  // Add circles representing the confirmed cases
-  svg
-    .selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", function (d) {
-      return xScale(d.lat);
-    })
-    .attr("cy", function (d) {
-      return yScale(d.long);
-    })
-    .attr("r", 10) // Circle size
-    .style("fill", "blue") // Color of circles
-    .style("opacity", 0.7) // Opacity
-    .on("mouseover", function (d) {
-      info.style("display", "block");
-      info
-        .html(
-          `Country: ${d.country}<br>Confirmed Cases: ${d.confirmed}<br>Latitude: ${d.lat}<br>Longitude: ${d.long}`
-        )
-        .style("left", d3.event.pageX + 10 + "px")
-        .style("top", d3.event.pageY - 28 + "px");
-    })
-    .on("mouseout", function () {
-      info.style("display", "none");
-    });
+  // svg
+  //   .selectAll("circle")
+  //   .data(data)
+  //   .enter()
+  //   .append("circle")
+  //   .attr("cx", function (d) {
+  //     return xScale(d.lat);
+  //   })
+  //   .attr("cy", function (d) {
+  //     return yScale(d.long);
+  //   })
+  //   .attr("r", 10) // Circle size
+  //   .style("fill", "blue") // Color of circles
+  //   .style("opacity", 0.7) // Opacity
+  //   .on("mouseover", function (d) {
+  //     info.style("display", "block");
+  //     info
+  //       .html(
+  //         `Country: ${d.country}<br>Confirmed Cases: ${d.confirmed}<br>Latitude: ${d.lat}<br>Longitude: ${d.long}`
+  //       )
+  //       .style("left", d3.event.pageX + 10 + "px")
+  //       .style("top", d3.event.pageY - 28 + "px");
+  //   })
+  //   .on("mouseout", function () {
+  //     info.style("display", "none");
+  //   });
 }
