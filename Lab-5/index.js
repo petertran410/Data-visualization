653c35ca3d9a0d06216e6e785064ab17a47059dd
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2023-11-09 16:40:16 +0700

    .

--- Lab-5/index.js
+++ Lab-5/index.js
@@ -59,31 +59,33 @@ function draw(data) {
 
   let xScale = d3
     .scaleLinear()
-    .domain([minLat, maxLat])
+    .domain([minLong, maxLong])
     .range([0, width - spacing]);
   let yScale = d3
     .scaleLinear()
-    .domain([minLong, maxLong])
+    .domain([minLat, maxLat])
     .range([height - spacing, 0]);
 
   var xAxis = d3.axisBottom(xScale);
   var yAxis = d3.axisLeft(yScale);
 
-  // svg
-  //   .append("g")
-  //   .attr("class", "grid")
-  //   .attr("transform", `translate(0, ${height - spacing / 2})`)
-  //   .call(
-  //     d3
-  //       .axisBottom(xScale)
-  //       .tickSize(-height + spacing / 2)
-  //       .tickFormat("")
-  //       .tickSizeOuter(0)
-  //       .tickSizeInner(-height + spacing / 2)
-  //       .tickPadding(10)
-  //       .tickFormat("")
-  //   );
+  // 1
+    svg
+      .append("g")
+      .attr("class", "grid")
+      .attr("transform", `translate(0, ${height - spacing / 2})`)
+      .call(
+        d3
+          .axisBottom(xScale)
+          .tickSize(-height + spacing / 2)
+          .tickFormat("")
+          .tickSizeOuter(0)
+          .tickSizeInner(-height + spacing / 2)
+          .tickPadding(10)
+          .tickFormat("")
+      );
 
+  // 2
   svg
     .append("g")
     .attr("class", "grid")
@@ -99,58 +101,58 @@ function draw(data) {
         .tickFormat("")
     );
 
-  // svg
-  //   .append("g")
-  //   .attr("id", "xAxis")
-  //   .attr("transform", `translate(0, ${(height - spacing) / 2})`)
-  //   .call(xAxis)
-  //   .selectAll("text")
-  //   .style("font-size", "14px")
-  //   .style("font-weight", "bold")
-  //   .filter(function (d) {
-  //     return d === 0;
-  //   })
-  //   .attr("transform", "translate(-10,0)")
-  //   .style("text-anchor", "end");
+  svg
+    .append("g")
+    .attr("id", "xAxis")
+    .attr("transform", `translate(0, ${(height - spacing) / 2})`)
+    .call(xAxis)
+    .selectAll("text")
+    .style("font-size", "14px")
+    .style("font-weight", "bold")
+    .filter(function (d) {
+      return d === 0;
+    })
+    .attr("transform", "translate(-10,0)")
+    .style("text-anchor", "end");
 
-  // svg
-  //   .append("g")
-  //   .attr("id", "yAxis")
-  //   .attr("transform", `translate(${(width - spacing) / 2}, 0)`)
-  //   .call(yAxis)
-  //   .selectAll("text")
-  //   .style("font-size", "14px")
-  //   .style("font-weight", "bold")
-  //   .attr("transform", "translate(0,-3)")
-  //   .filter(function (d) {
-  //     return d === 0;
-  //   })
-  //   .remove();
+  svg
+    .append("g")
+    .attr("id", "yAxis")
+    .attr("transform", `translate(${(width - spacing) / 2}, 0)`)
+    .call(yAxis)
+    .selectAll("text")
+    .style("font-size", "14px")
+    .style("font-weight", "bold")
+    .attr("transform", "translate(0,-3)")
+    .filter(function (d) {
+      return d === 0;
+    })
+    .remove();
 
-  // svg
-  //   .selectAll("circle")
-  //   .data(data)
-  //   .enter()
-  //   .append("circle")
-  //   .attr("cx", function (d) {
-  //     return xScale(d.lat);
-  //   })
-  //   .attr("cy", function (d) {
-  //     return yScale(d.long);
-  //   })
-  //   .attr("r", 10) // Circle size
-  //   .style("fill", "blue") // Color of circles
-  //   .style("opacity", 0.7) // Opacity
-  //   .on("mouseover", function (d) {
-  //     info.style("display", "block");
-  //     info
-  //       .html(
-  //         `Country: ${d.country}<br>Confirmed Cases: ${d.confirmed}<br>Latitude: ${d.lat}<br>Longitude: ${d.long}`
-  //       )
-  //       .style("left", d3.event.pageX + 10 + "px")
-  //       .style("top", d3.event.pageY - 28 + "px");
-  //   })
-  //   .on("mouseout", function () {
-  //     info.style("display", "none");
-  //   });
+  svg
+    .selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) {
+      return xScale(d.long);
+    })
+    .attr("cy", function (d) {
+      return yScale(d.lat);
+    })
+    .attr("r", 10) // Circle size
+    .style("fill", "blue") // Color of circles
+    .style("opacity", 0.7) // Opacity
+    .on("mouseover", function (d) {
+      info.style("display", "block");
+      info
+        .html(
+          `Country: ${d.country}<br>Confirmed Cases: ${d.confirmed}<br>Latitude: ${d.lat}<br>Longitude: ${d.long}`
+        )
+        .style("left", d3.event.pageX + 10 + "px")
+        .style("top", d3.event.pageY - 28 + "px");
+    })
+    .on("mouseout", function () {
+      info.style("display", "none");
+    });
 }
