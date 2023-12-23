42c0510db575bdd655c89efed5df2280172fe798
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2023-12-23 15:29:30 +0700

    .

--- Lab-8/TranNgocNhan-ITITIU20264-Lab08/vietnam-covid.js
+++ Lab-8/TranNgocNhan-ITITIU20264-Lab08/vietnam-covid.js
@@ -47,96 +47,100 @@ var tooltip = d3
   .attr("class", "tooltip")
   .style("opacity", 0);
 
-d3.csv("covid19_province.csv", function (data) {
-  colorScale.domain([
-    d3.min(data, function (d) {
-      return d.Confirm;
-    }),
-    d3.max(data, function (d) {
-      return d.Confirm;
-    }),
-  ]);
+d3.csv(
+  "https://raw.githubusercontent.com/petertran410/Data-visualization/main/Lab-8/TranNgocNhan-ITITIU20264-Lab08/covid19_province.csv",
+  function (data) {
+    colorScale.domain([
+      d3.min(data, function (d) {
+        return d.Confirm;
+      }),
+      d3.max(data, function (d) {
+        return d.Confirm;
+      }),
+    ]);
 
-  d3.json(
-    "https://raw.githubusercontent.com/TungTh/tungth.github.io/master/data/vn-provinces.json",
-    function (json) {
-      for (var i = 0; i < data.length; i++) {
-        var dataProvince = parseFloat(data[i].ma);
-        console.log(json.features[0].properties.Ma);
+    d3.json(
+      "https://raw.githubusercontent.com/TungTh/tungth.github.io/master/data/vn-provinces.json",
+      function (json) {
+        for (var i = 0; i < data.length; i++) {
+          var dataProvince = parseFloat(data[i].ma);
+          console.log(json.features[0].properties.Ma);
 
-        var dataC = data[i].Confirm;
+          var dataC = data[i].Confirm;
 
-        for (var j = 0; j < json.features.length; j++) {
-          var jsonProvince = json.features[j].properties.Ma;
+          for (var j = 0; j < json.features.length; j++) {
+            var jsonProvince = json.features[j].properties.Ma;
+            if (dataProvince == jsonProvince) {
+              json.features[j].properties.cases = dataC;
+              console.log(dataC);
+              break;
+            }
+          }
+          var dataName = data[i].Province;
 
-          if (dataProvince == jsonProvince) {
-            json.features[j].properties.cases = dataC;
+          for (var k = 0; k < json.features.length; k++) {
+            var jsonProvince = json.features[k].properties.Ma;
 
-            break;
+            if (dataProvince == jsonProvince) {
+              json.features[k].properties.province = dataName;
+
+              break;
+            }
           }
         }
-        var dataName = data[i].Province;
 
-        for (var k = 0; k < json.features.length; k++) {
-          var jsonProvince = json.features[k].properties.Ma;
+        var map = g
+          .selectAll("path")
+          .data(json.features)
+          .enter()
+          .append("path")
+          .attr("d", path)
+          .style("fill", function (d) {
+            var value = d.properties.cases;
+            if (value > 0) {
+              return colorScale(value);
+            } else {
+              return "#ccc";
+            }
+          })
+          .on("mouseover", function (d) {
+            d3.select(this)
+              .transition()
+              .duration(200)
+              .style("opacity", 1)
+              .style("stroke", "black");
+            tooltip
+              .transition()
+              .duration(200)
+              .style("opacity", 0.9)
+              .style("stroke", "black");
+            tooltip.html(d.province);
+          })
+          .on("mousemove", function (d) {
+            tooltip
+              .style("top", d3.event.pageY + "px")
+              .style("left", d3.event.pageX + 10 + "px");
 
-          if (dataProvince == jsonProvince) {
-            json.features[k].properties.province = dataName;
-
-            break;
-          }
-        }
+            tooltip.html(
+              d.properties.province.bold() +
+                " <br>Cases: " +
+                d.properties.cases.bold()
+            );
+          })
+          .on("mouseout", function (d) {
+            d3.select(this)
+              .transition()
+              .duration(200)
+              .style("stroke", "transparent");
+            tooltip.transition().duration(200).style("opacity", 0);
+          });
       }
-
-      var map = g
-        .selectAll("path")
-        .data(json.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .style("fill", function (d) {
-          var value = d.properties.cases;
-          if (value > 0) {
-            return colorScale(value);
-          } else {
-            return "#ccc";
-          }
-        })
-        .on("mouseover", function (d) {
-          d3.select(this)
-            .transition()
-            .duration(200)
-            .style("opacity", 1)
-            .style("stroke", "black");
-          tooltip
-            .transition()
-            .duration(200)
-            .style("opacity", 0.9)
-            .style("stroke", "black");
-          tooltip.html(d.province);
-        })
-        .on("mousemove", function (d) {
-          tooltip
-            .style("top", d3.event.pageY + "px")
-            .style("left", d3.event.pageX + 10 + "px");
-
-          tooltip.html(
-            d.properties.province.bold() +
-              " <br>Cases: " +
-              d.properties.cases.bold()
-          );
-        })
-        .on("mouseout", function (d) {
-          d3.select(this)
-            .transition()
-            .duration(200)
-            .style("stroke", "transparent");
-          tooltip.transition().duration(200).style("opacity", 0);
-        });
-    }
-  );
-});
+    );
+  }
+);
 
 function zoomed() {
   g.selectAll("path").attr("transform", d3.event.transform);
 }
+
+
